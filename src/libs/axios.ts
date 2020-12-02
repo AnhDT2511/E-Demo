1a1acf6a33be437b624f101d21eb98008aa9a81e
AnhDT106 <AnhDT106@fpt.com.vn>
2020-12-02 15:09:46 +0700

    fix

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //const baseURL = "http://face-api.aeyes.tech";
 //const baseURL = "https://973162dea8aa.ngrok.io";
-const baseURL = "http://localhost:8089";
+const baseURL = "http://localhost:8198";
 
 // ALL DEFAULT CONFIGURATION HERE
 export const anestAxios = axios.create({
