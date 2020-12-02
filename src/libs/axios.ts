3c2a5a8ca5e10538ebb5a4582739b9ce66670060
AnhDT106 <AnhDT106@fpt.com.vn>
2020-12-02 15:06:07 +0700

    add url

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 //const baseURL = "http://face-api.aeyes.tech";
-const baseURL = "https://973162dea8aa.ngrok.io";
+//const baseURL = "https://973162dea8aa.ngrok.io";
+const baseURL = "http://localhost:8089";
 
 // ALL DEFAULT CONFIGURATION HERE
 export const anestAxios = axios.create({
